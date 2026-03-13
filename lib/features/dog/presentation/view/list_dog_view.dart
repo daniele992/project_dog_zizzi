a7640c647ff01ab2cd74c2ac3b8197d81be511b2
daniele992 <moranidaniele129@yahoo.it>
2026-03-13 19:44:09 +0100

    modifiche per la lista dei cani

--- lib/features/dog/presentation/view/list_dog_view.dart
+++ lib/features/dog/presentation/view/list_dog_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../../core/providers/authRepository/user_provider.dart';
 import '../../../../core/providers/dog/dog_providers.dart';
 
 class ListDogs extends ConsumerStatefulWidget{
@@ -16,14 +17,36 @@ class _ListDogs extends ConsumerState<ListDogs>{
   void initState(){
     super.initState();
     Future.microtask((){
-      ref.read(dogListViewModelProvider.notifier).fetchDogs(userId: 1, isAdmin: true);
+      final isAdmin = ref.read(userIsAdminProvider).value;
+      if(isAdmin != null){ //TODO modificare in seguito
+        ref.read(dogListViewModelProvider.notifier).fetchDogs(userId: 1, isAdmin: true);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: Text("Lista cani"),
+
+    final isAdmin = ref.watch(userIsAdminProvider).value;
+    final dogsState = ref.watch(dogListViewModelProvider);
+
+    return Scaffold(
+      body: dogsState.when(
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (e, _) => Center(child: Text("Errore: $e")),
+        data: (dogs) {
+          return ListView.builder(
+            itemCount: dogs.length,
+            itemBuilder: (context, index) {
+              final dog = dogs[index];
+              return ListTile(
+                title: Text(dog.name),
+                subtitle: Text(dog.breed),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
