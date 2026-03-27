d45191ce4ac8093c4c11994315cd7a7d6c57281b
daniele992 <moranidaniele129@yahoo.it>
2026-03-27 18:42:52 +0100

    aggiunta classe JwtClaims e modifica per il recupero id dal token

---
+++ lib/core/constants/jwt_claims.dart
@@ -0,0 +1,7 @@
+class JwtClaims {
+  static const userId = 'userId';
+  static const name = 'name';
+  static const surname = 'surname';
+  static const email = 'email';
+  static const admin = 'admin';
+}
--- lib/core/utils/jwt/jwt_utils.dart
+++ lib/core/utils/jwt/jwt_utils.dart
@@ -1,4 +1,5 @@
 import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:project_dog_zizzi/core/constants/jwt_claims.dart';
 
 class JwtUtils {
   ///Funzione per recuperare userName dal token
@@ -6,8 +7,8 @@ class JwtUtils {
     if (JwtDecoder.isExpired(token)) return null;
     final decoded = JwtDecoder.decode(token);
 
-    final name = decoded['name'] as String?;
-    final surname = decoded['surname'] as String?;
+    final name = decoded[JwtClaims.name] as String?;
+    final surname = decoded[JwtClaims.surname] as String?;
 
     if (name == null || surname == null) return null;
 
@@ -18,7 +19,7 @@ class JwtUtils {
   static bool isAdminFromToken(String token) {
     if (JwtDecoder.isExpired(token)) return false;
     final decoded = JwtDecoder.decode(token);
-    final adminValue = decoded['admin'];
+    final adminValue = decoded[JwtClaims.admin];
     if (adminValue == null) return false;
     if (adminValue is bool) return adminValue;
 
@@ -37,13 +38,14 @@ class JwtUtils {
 
     //final userIdValue = decoded['userId'];
 
-    //Spesso nei JWT L'id non si chiama ownerId. Quindi questa è una versione più robusta
+    /*Spesso nei JWT L'id non si chiama ownerId. Quindi questa è una versione più robusta
     final userIdValue =
         decoded['ownerId'] ??
         decoded['nameid'] ??
         decoded['sub'] ??
         decoded['id'] ??
-        decoded['userId'];
+        decoded['userId']; */
+    final userIdValue = decoded[JwtClaims.userId];
 
     if (userIdValue == null) return null;
 
