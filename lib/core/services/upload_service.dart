ee59cabbf05834c227110997ca26ac1e43074071
daniele992 <moranidaniele129@yahoo.it>
2026-05-05 16:19:29 +0200

    Modificato Upload Service

--- lib/core/services/upload_service.dart
+++ lib/core/services/upload_service.dart
@@ -2,40 +2,21 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:image_picker/image_picker.dart';
 
+import '../constants/api_constants.dart';
+
 class UploadService {
   final Dio _dio = Dio();
 
-  Future<String?> uploadImage({
-    required XFile file,
-    required String token,
-    required Function (double progress) onProgress,
-  }) async {
-
-    final bytes = await file.readAsBytes();
+  Future<String> uploadImage(XFile file) async {
     final formData = FormData.fromMap({
-      "image" : MultipartFile.fromBytes(
-        bytes,
-        filename: file.name,
-      ),
+      "image": await MultipartFile.fromFile(file.path),
     });
 
     final response = await _dio.post(
-      "http://localhost:5059/api/dogs",
+      "${ApiConstants.baseUrl}/upload",
       data: formData,
-      options: Options(
-        headers: {"Authorization": "Bearer $token"},
-      ),
-      onSendProgress: (sent, total) {
-        final progress = sent / total;
-        onProgress(progress);
-      },
     );
 
-    if(response.statusCode == 200){
-      return response.data["image"];
-    }
-
-    return null;
-
+    return response.data["imageUrl"];
   }
 }
